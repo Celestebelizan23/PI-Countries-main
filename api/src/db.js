aa2bcba1e477a433821edba5b67dc23e1c705b02
Celestebelizan23 <celestebelizan23@gmail.com>
2023-02-05 17:42:11 -0300

    restructuring code

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT,} = process.env;
+  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT,PGDATABASE } = process.env;
 
 const sequelize = new Sequelize
 (`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}${PGDATABASE}/countries`, {
