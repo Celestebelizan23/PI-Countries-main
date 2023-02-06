e5d40d3797de9ec6cd1cd11cc5aa22e27dad75ad
Celestebelizan23 <celestebelizan23@gmail.com>
2023-02-05 21:18:21 -0300

    restructuring code

--- api/index.js
+++ api/index.js
@@ -43,8 +43,9 @@ async function getCountries(){
 
 //Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    getCountries()
+    console.log('%s listening at', process.env.PORT); // eslint-disable-line no-console
   });
 });
 
