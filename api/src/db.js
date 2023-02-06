9c5bc935cb828bc84f27e71eee17ee76ed82fb55
Celestebelizan23 <celestebelizan23@gmail.com>
2023-02-05 22:33:11 -0300

    restructuring code

--- api/src/db.js
+++ api/src/db.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_NAME, BD_HOST, DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
-
-
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${BD_NAME}`,
+{
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+}
+);
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
